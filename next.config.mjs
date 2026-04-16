afabb032e095c4c1712b1017c10d8d4982ad6b50
Sangyoon <hobbus0117@gmail.com>
2026-04-16 11:19:33 +0900

    fix:  Content-Security-Policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [{ key: "Content-Security-Policy", value: "" }],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
